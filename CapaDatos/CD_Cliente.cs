cfe63d72bb636c96b01e6d9c5eeaeed27782beca
victr974 <vcortezs@miumg.edu.gt>
2022-12-09 15:01:00 -0600

    Clase Cliente creado en CapaDatos

---
+++ CapaDatos/CD_Cliente.cs
@@ -0,0 +1,146 @@
+﻿using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class CD_Cliente
+    {
+
+        public int Registrar(Cliente obj, out string Mensaje)
+        {
+            int idautogenerado = 0;
+            Mensaje = string.Empty;
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cn))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_RegistrarCliente", oconexion);
+                    cmd.Parameters.AddWithValue("Nombres", obj.nombres);
+                    cmd.Parameters.AddWithValue("Apellidos", obj.apellidos);
+                    cmd.Parameters.AddWithValue("Correo", obj.correo);
+                    cmd.Parameters.AddWithValue("Clave", obj.clave);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    oconexion.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    idautogenerado = Convert.ToInt32(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                idautogenerado = 0;
+                Mensaje = ex.Message;
+            }
+
+            return idautogenerado;
+        }
+
+        public List<Cliente> Listar()
+        {
+            List<Cliente> lista = new List<Cliente>();
+
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cn))
+                {
+                    string query = "select idCliente, nombres, apellidos, correo, clave, reestablecer from Cliente";
+                    SqlCommand cmd = new SqlCommand(query, oconexion);
+                    cmd.CommandType = CommandType.Text;
+
+                    oconexion.Open();
+
+                    using (SqlDataReader rdr = cmd.ExecuteReader())
+                    {
+                        while (rdr.Read())
+                        {
+                            lista.Add(
+                                new Cliente()
+                                {
+                                    idCliente = Convert.ToInt32(rdr["idCliente"]),
+                                    nombres = rdr["nombres"].ToString(),
+                                    apellidos = rdr["apellidos"].ToString(),
+                                    correo = rdr["correo"].ToString(),
+                                    clave = rdr["clave"].ToString(),
+                                    reestablecer = Convert.ToBoolean(rdr["reestablecer"]),
+                                }
+
+                                );
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                lista = new List<Cliente>();
+            }
+
+            return lista;
+        }
+
+        public bool CambiarClave(int idcliente, string nuevaclave, out string Mensaje)
+        {
+            bool resultado = false;
+            Mensaje = string.Empty;
+
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cn))
+                {
+                    SqlCommand cmd = new SqlCommand("update  Cliente set clave = @nuevaclave, reestablecer = 0 where idCliente = @id", oconexion);
+                    cmd.Parameters.AddWithValue("@id", idcliente);
+                    cmd.Parameters.AddWithValue("@nuevaclave", nuevaclave);
+                    cmd.CommandType = CommandType.Text;
+                    oconexion.Open();
+                    resultado = cmd.ExecuteNonQuery() > 0 ? true : false;
+                }
+            }
+            catch (Exception ex)
+            {
+                resultado = false;
+                Mensaje = ex.Message;
+
+            }
+            return resultado;
+
+        }
+
+        public bool ReestablecerClave(int idcliente, string clave, out string Mensaje)
+        {
+            bool resultado = false;
+            Mensaje = string.Empty;
+
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cn))
+                {
+                    SqlCommand cmd = new SqlCommand("update  Cliente set clave = @clave, reestablecer = 1 where idCliente = @id", oconexion);
+                    cmd.Parameters.AddWithValue("@id", idcliente);
+                    cmd.Parameters.AddWithValue("@clave", clave);
+                    cmd.CommandType = CommandType.Text;
+                    oconexion.Open();
+                    resultado = cmd.ExecuteNonQuery() > 0 ? true : false;
+                }
+            }
+            catch (Exception ex)
+            {
+                resultado = false;
+                Mensaje = ex.Message;
+
+            }
+            return resultado;
+
+        }
+
+    }
+}
