973a6738a6b5266c110645eb23f78c4529ebc9eb
victr974 <vcortezs@miumg.edu.gt>
2022-12-09 19:44:53 -0600

    Clase Ubicacion creado en CapaNegocio

---
+++ CapaNegocio/CN_Ubicacion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using CapaDatos;
+using CapaEntidad;
+
+
+namespace CapaNegocio
+{
+    public class CN_Ubicacion
+    {
+        private CD_Ubicacion objCapaDato = new CD_Ubicacion();
+
+        public List<Departamento> obtenerDepartamento()
+        {
+            return objCapaDato.obtenerDepartamento();
+        }
+
+        public List<Municipio> obenerMunicipio(string iddepartamento)
+        {
+            return objCapaDato.obenerMunicipio(iddepartamento);
+        }
+
+        public List<Zona> obtenerZona(string idmunicipio, string iddepartamento)
+        {
+            return objCapaDato.obtenerZona(idmunicipio, iddepartamento);
+        }
+    }
+}
